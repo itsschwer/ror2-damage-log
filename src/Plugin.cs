d1dbdfee7388dd55bb22bfff00e56c16a6e43ea1
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-01-01 17:16:50 +1100

    Bump version

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -11,7 +11,7 @@ namespace DamageLog
         public const string GUID = Author + "." + Name;
         public const string Author = "itsschwer";
         public const string Name = "DamageLog";
-        public const string Version = "0.0.0";
+        public const string Version = "0.1.0";
 
         internal static new Config Config { get; private set; }
         internal static void ReloadConfig() => RequestConfigReload?.Invoke();
