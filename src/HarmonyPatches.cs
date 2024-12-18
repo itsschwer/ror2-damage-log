84658df24b2b98894943b1ddc0c8b2e5860ee633
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-12-18 15:46:27 +1100

    Wrap harmony patch method bodies in try-catch

--- src/HarmonyPatches.cs
+++ src/HarmonyPatches.cs
@@ -6,18 +6,24 @@ namespace DamageLog
     internal static class HarmonyPatches
     {
         [HarmonyPostfix, HarmonyPatch(typeof(RoR2.UI.GameEndReportPanelController), nameof(RoR2.UI.GameEndReportPanelController.Awake))]
-        private static void GameEndReportPanelController_Awake(RoR2.UI.GameEndReportPanelController __instance)
-            => DamageLogUI.MoveToGameEndReportPanel(__instance);
+        private static void GameEndReportPanelController_Awake(RoR2.UI.GameEndReportPanelController __instance) {
+            try { DamageLogUI.MoveToGameEndReportPanel(__instance); }
+            catch (System.Exception e) { Plugin.Logger.LogError(e); }
+        }
 
         [HarmonyPostfix, HarmonyPatch(typeof(RoR2.UI.GameEndReportPanelController), nameof(RoR2.UI.GameEndReportPanelController.SetPlayerInfo))]
-        private static void GameEndReportPanelController_SetPlayerInfo(RoR2.RunReport.PlayerInfo playerInfo)
-            => DamageLogUI.DisplayPlayerDamageLog(playerInfo?.networkUser);
+        private static void GameEndReportPanelController_SetPlayerInfo(RoR2.RunReport.PlayerInfo playerInfo) {
+            try { DamageLogUI.DisplayPlayerDamageLog(playerInfo?.networkUser); }
+            catch (System.Exception e) { Plugin.Logger.LogError(e); }
+        }
 
 
 
 
         [HarmonyPostfix, HarmonyPatch(typeof(RoR2.BossGroup), nameof(RoR2.BossGroup.OnMemberDiscovered))]
-        private static void BossGroup_OnMemberDiscovered(RoR2.BossGroup __instance, RoR2.CharacterMaster memberMaster)
-            => Plugin.TrackBoss(__instance, memberMaster);
+        private static void BossGroup_OnMemberDiscovered(RoR2.BossGroup __instance, RoR2.CharacterMaster memberMaster) {
+            try { Plugin.TrackBoss(__instance, memberMaster); }
+            catch (System.Exception e) { Plugin.Logger.LogError(e); }
+        }
     }
 }
